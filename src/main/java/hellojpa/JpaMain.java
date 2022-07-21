a53c8e20091fa9c58fccb3b1c82d65d4155dd7f2
hyse <gustmd4296@hanmail.net>
2022-07-21 11:17:51 +0900

    7.21(목)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,21 +19,13 @@ public class JpaMain {
         tx.begin() ;
 
         try {
-            Child child1 = new Child();
-            Child child2 = new Child();
 
-            Parent parent = new Parent();
-            parent.addChild(child1);
-            parent.addChild(child2);
+            Member member = new Member();
+            member.setUsername("Hello");
+            member.setHomeAddress(new Address("city","street","zipcode"));
+            member.setWorkPeriod(new Period());
 
-            em.persist(parent);
-            em.persist(child1);
-            em.persist(child2);
-            em.flush();
-            em.close();
-
-            Parent findParent = em.find(Parent.class ,parent.getId());
-            findParent.getChildList().remove(0);
+            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
@@ -44,15 +36,6 @@ public class JpaMain {
         }
         emf.close();
 
-    }
-
-  
-
-    private static Member saveMember(EntityManager em) {
-        Member member = new Member();
-        member.setUsername("member1");
-        em.persist(member);
-        return member;
     }
 
 
